ce34cd3f1ef5a536108460ffc443a40f4e47dc54
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-18 16:11:16 +0200

    add first size of "snack2.3"

---
+++ jsnack3/script.js
@@ -0,0 +1,24 @@
+/*
+Snack 2.3
+Crea un array di numeri interi 
+Fai la somma di tutti gli elementi che sono in posizione dispari
+*/
+
+// creare un arrey con numeri interi
+
+let number = [
+    3,
+    8,
+    89,
+    53,
+    11,
+    22,
+    56,
+    99,
+    80,
+    1
+]
+
+for(number=1; number < number.length; number + 2){
+    console.log(number);
+}
