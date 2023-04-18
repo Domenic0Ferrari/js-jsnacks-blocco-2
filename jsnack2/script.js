cb7fd84a75ff9aed0ffd1c37c3bdf197788f63f3
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-18 15:54:37 +0200

    add exercize "jsnack2.2"

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,9 +6,9 @@ Da queste vuole generare una falsa lista di invitati con nome e cognome.
 */
 
 // fare le due liste (nomi) e (cognomi)
-// prendere un elmento a caso dalla lista uno e aggiungere un elemento a caso dalla lista due
 
-let name = [
+
+let nome = [
     "Antonio",
     "Gianluca",
     "Enrico",
@@ -26,13 +26,14 @@ let surname = [
     "Barbati"
 ];
 
-for (i=0; i < name.length; i++){
-    console.log(name[i]);
+// prendere elementi a caso dalla lista uno e due e sommarli
+
+for (i=0; i < 10; i++){
+    let nomeRandom = Math.floor(Math.random()*nome.length);
+    let cognomeRandom = Math.floor(Math.random()*surname.length);
+    console.log(nome[nomeRandom] + surname[cognomeRandom]);
 }
 
-for (i=0; i < surname.length; i++){
-    console.log(surname[i]);
-}
 
-console.log(name[i] + surname[i]);
+
 
